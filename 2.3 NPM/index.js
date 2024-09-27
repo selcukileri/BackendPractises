79f361c29e616be06f0fd60a6a50e1f6ce38c60a
Selçuk İleri <selcukileri98@gmail.com>
2024-09-27 15:33:25 +0300

    some backend practises

---
+++ 2.3 NPM/index.js
@@ -0,0 +1,8 @@
+// import generateName from "sillyname"
+// var sillyName = generateName();
+
+// console.log(`My name is ${sillyName}.`);
+
+import superheroes, { randomSuperhero } from "superheroes"
+const name = randomSuperhero();
+console.log(`I am ${name}!`);
